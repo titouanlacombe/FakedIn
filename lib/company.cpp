1556d2a6b5b15fe558d105a9cb2068008571eb61
P-R-O-O-F <36766011+P-R-O-O-F@users.noreply.github.com>
2020-05-29 09:58:37 +0200

    company fill

--- lib/company.cpp
+++ lib/company.cpp
@@ -2,15 +2,19 @@
 
 Company::Company()
 {
-	
+	email = "";
+  zip_code = "";
+  name = "";
 }
 
 Company::Company(std::string _name, std::string _zip_code, std::string _email)
 {
-	
+	email = _email;
+  zip_code = _zip_code;
+  name = _name;
 }
 
 Company::~Company()
 {
-
+  
 }
