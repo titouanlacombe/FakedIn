43ed6115eaa405c5c5fc3d0432fa969b5a282429
titouanlacombe <titouan.lacombe99@gmail..com>
2020-05-29 10:19:13 +0200

    worker implemented

--- lib/worker.cpp
+++ lib/worker.cpp
@@ -2,40 +2,53 @@
 
 Worker::Worker()
 {
-
+	first_name = "";
+	last_name = "";
+	email = "";
+	zip_code = "";
+	skills = NULL;
+	colleagues = NULL;
+	company = NULL;
 }
 
 Worker::Worker(std::string _first_name, std::string _last_name, std::string _email)
 {
-
+	first_name = _first_name;
+	last_name = _last_name;
+	email = _email;
+	zip_code = "";
+	skills = new List<std::string>();
+	colleagues = new List<Worker*>();
+	company = NULL;
 }
 
 Worker::~Worker()
 {
-
+	delete skills;
+	delete colleagues;
 }
 
 void Worker::add_skill(std::string skill)
 {
-
+	skills->addlast(skill);
 }
 
 void Worker::add_colleague(Worker* colleague)
 {
-
+	colleagues->addlast(colleague);
 }
 
-void Worker::set_zip_code(std::string zip_code)
+void Worker::set_zip_code(std::string _zip_code)
 {
-
+	zip_code = _zip_code;
 }
 
-void Worker::set_company(Company* company)
+void Worker::set_company(Company* _company)
 {
-
+	company = _company;
 }
 
 bool Worker::employed()
 {
-	return false;
+	return company != NULL;
 }
